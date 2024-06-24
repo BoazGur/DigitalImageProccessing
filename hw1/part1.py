27a94f64d59d53e231441adaa9f0897104e7404c
Boaz Gurevich <boazirena@gmail.com>
2024-06-24 22:34:34 +0300

    added documentation and redirected the answer images to answers/

--- hw1/part1.py
+++ hw1/part1.py
@@ -3,13 +3,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def bilinear_interpolation(img):
+    # initialize parameters
     n, m = img.shape
     interpolated_pic = np.zeros((2 * n, 2 * m))
-    
+
+    # fill original image into inetrpolated image at even places
     for i in range(n):
         for j in range(m):
             interpolated_pic[2 * i, 2 * j] = img[i,j]
 
+    # fill all the odd places in even rows
     for i in range(0, 2 * n, 2):
         for j in range(1, 2 * m, 2):
             if j == 2 * m - 1:
@@ -17,6 +20,7 @@ def bilinear_interpolation(img):
             else:
                 interpolated_pic[i, j] = np.mean([interpolated_pic[i, j - 1], interpolated_pic[i, j + 1]])
 
+    # fill all the odd rows
     for j in range(0, 2 * m):  
         for i in range(1, 2 * n, 2):
             if i == 2 * n - 1:
@@ -27,70 +31,98 @@ def bilinear_interpolation(img):
     return interpolated_pic
 
 def save_hist(img, name):
+    # flatten img for use of plt.hist
     flatten_img = img.flatten()
+
+    # plot the histogram
     plt.hist(flatten_img, bins=255)
-    plt.title('Leafs histogram')
+    plt.title(f'{name} histogram')
     plt.xlabel('Intensity')
     plt.ylabel('Frequency')
-    plt.savefig(name + '.png')
+    plt.savefig('answers/' + name + '.png')
+    
+    # clear figure for later use
     plt.clf()
 
 def strech_hist(img):
     min_freq = np.min(img)
     max_freq = np.max(img)
 
+    # strech the image's histogram
     return 255 * ((img - min_freq) / (max_freq - min_freq))
 
 def equalize_hist(img):
+    # get histogram
     n, m = img.shape
     hist = np.histogram(img, bins=256)[0]
-    probability = hist / (n * m)
     
+    # get the cdf of the histogram
+    probability = hist / (n * m)
     cdf = np.cumsum(probability)
     
+    # equalize histogram
     return np.round(255 * cdf)
 
 def equalize_img(img):
+    # equalize histogram
     equalized_hist = equalize_hist(img)
+    
+    # create a lambda function that uses the equalized histogram to equalize the image
     equalized_img_lambda = lambda x: equalized_hist[x.astype(int)]
 
+    # equalize the image
     return equalized_img_lambda(img)
 
 def main():
     ######################### PART 1 - 1 ##################################
-    # img = Image.open('./hw12024_input_img/hw12024/peppers.jpg')
-    # img = ImageOps.grayscale(img)
-
-    # interpolated_peppers = bilinear_interpolation(np.asarray(img))
     
-    # image = Image.fromarray(interpolated_peppers).convert('L')
-    # image.save('InterpolatedPeppersX2.png')
-
-    # interpolated_peppers = bilinear_interpolation(interpolated_peppers)
-    # interpolated_peppers = bilinear_interpolation(interpolated_peppers)
-
-    # image = Image.fromarray(interpolated_peppers).convert('L')
-    # image.save('InterpolatedPeppersX8.png')
-
-    ######################### PART 1 - 2 ##################################
-    img = Image.open('./hw12024_input_img/hw12024/leafs.jpg')
+    # open peppers photo into greyscale image
+    img = Image.open('hw12024_input_img/hw12024/peppers.jpg')
     img = ImageOps.grayscale(img)
 
-    # save_hist(np.asarray(img), 'before_flattening')
+    # interpolate as asked
+    interpolated_peppers = bilinear_interpolation(np.asarray(img))
+    
+    # save X2 super resolution image
+    image = Image.fromarray(interpolated_peppers).convert('L')
+    image.save('answers/InterpolatedPeppersX2.jpg')
 
+    # interpolate to get X8 resolution image
+    interpolated_peppers = bilinear_interpolation(interpolated_peppers)
+    interpolated_peppers = bilinear_interpolation(interpolated_peppers)
+
+    # save
+    image = Image.fromarray(interpolated_peppers).convert('L')
+    image.save('answers/InterpolatedPeppersX8.jpg')
+
+    ######################### PART 1 - 2 ##################################
+    
+    # open leafs image
+    img = Image.open('hw12024_input_img/hw12024/leafs.jpg')
+    img = ImageOps.grayscale(img)
+
+    # plot and save histogram
+    save_hist(np.asarray(img), 'before_flattening')
+
+    # strech histogram
     streched_img = strech_hist(np.asarray(img))
     
-    # save_hist(streched_img, 'after_streching')
+    # plot and save streched histogram
+    save_hist(streched_img, 'after_streching')
     
-    # streched_img = Image.fromarray(streched_img).convert('L')
-    # streched_img.save('streched_histogram_leafs.png')
+    # save image after streching histogram
+    pil_streched_img = Image.fromarray(streched_img).convert('L')
+    pil_streched_img.save('answers/streched_histogram_leafs.png')
 
+    # equalize image after streching
     equalized_img = equalize_img(streched_img)
 
+    # plot the histogram of the image
     save_hist(equalized_img, 'after_equalizing')
     
+    # save the equalized image
     equalized_img = Image.fromarray(equalized_img).convert('L')
-    equalized_img.save('equalized_histogram_leafs.png')
+    equalized_img.save('answers/equalized_histogram_leafs.png')
     
 
 if __name__ == '__main__':
--- hw1/part2.py
+++ hw1/part2.py
@@ -5,13 +5,15 @@ import cv2
 
 def conv2d(image, kernel):
         
-    # Padding to get original size
+    # padding size and kernel size
     k = kernel.shape[0]
     padding_width = k // 2
     
+    # initialization and padding
     padded_image = pad(image, padding_width)
     conved_image = np.zeros(image.shape)
 
+    # the convolution
     kernel_placements = [(i, i + image.shape[0], j, j + image.shape[1]) for i in range(k) for j in range(k)]
     for i_start, i_stop, j_start, j_stop in kernel_placements:
         conved_image += padded_image[i_start:i_stop, j_start:j_stop] * kernel[i_start, j_start]
@@ -19,77 +21,160 @@ def conv2d(image, kernel):
     return conved_image
 
 def pad(image, padding_width):
+    # pad with necessary padding for proper convolution
     padded = np.zeros((image.shape[0] + padding_width * 2, image.shape[1] + padding_width * 2))
     padded[padding_width:-padding_width, padding_width:-padding_width] = image
 
     return padded
 
+def calculate_amplitude_phase(path, factor=None, resize=False):
+    # import image
+    img = Image.open(path).convert('L')
+    img = np.array(img)
+
+    # crop the image if asked (useful for question 3d) 
+    if resize:
+        img = img[:factor[0],:factor[1]]
+    
+    # fft and shift
+    fft = np.fft.fft2(img)
+    fft_shifted = np.fft.fftshift(fft)
+
+    # retirive amplitude and phase
+    amplitude = np.abs(fft_shifted)
+    phase = np.angle(fft_shifted)
+
+    return amplitude, phase
+
 def main():
 
     ######################### PART 2 - 2a ##################################
 
-    # derivation_kernel = np.array([[1, 0, -1], [0, 0, 0], [-1, 0, 1]])
-   
-    # image1 = Image.open('./hw12024_input_img/hw12024/I.jpg')
-    # image1 = ImageOps.grayscale(image1)
+    # kernel of gradient
+    derivation_kernel = np.array([[1, 0, -1], [0, 0, 0], [-1, 0, 1]])
+
+    # load both images
+    image1 = Image.open('hw12024_input_img/hw12024/I.jpg')
+    image1 = ImageOps.grayscale(image1)
     
-    # image2 = Image.open('./hw12024_input_img/hw12024/I_n.jpg')
-    # image2 = ImageOps.grayscale(image2)
+    image2 = Image.open('hw12024_input_img/hw12024/I_n.jpg')
+    image2 = ImageOps.grayscale(image2)
 
-    # conved_img1 = conv2d(np.asarray(image1), derivation_kernel)
-    # conved_img2 = conv2d(np.asarray(image2), derivation_kernel)
+    # convolve with the gradiant
+    conved_img1 = conv2d(np.asarray(image1), derivation_kernel)
+    conved_img2 = conv2d(np.asarray(image2), derivation_kernel)
 
-    # conved_img1 = Image.fromarray(conved_img1).convert('L')
-    # conved_img1.save('derived_I.jpeg')
+    # export images
+    conved_img1 = Image.fromarray(conved_img1).convert('L')
+    conved_img1.save('answers/derived_I.jpg')
 
-    # conved_img2 = Image.fromarray(conved_img2).convert('L')
-    # conved_img2.save('derived_I_n.jpeg')
+    conved_img2 = Image.fromarray(conved_img2).convert('L')
+    conved_img2.save('answers/derived_I_n.jpg')
 
     ######################### PART 2 - 2b ##################################
- 
-    # I_n = Image.open('./hw12024_input_img/hw12024/I_n.jpg')
-    # I_n = ImageOps.grayscale(I_n)
-
-    # I_dn = cv2.GaussianBlur(np.asarray(I_n), (3, 3), 0)
-
-    # I_dn = Image.fromarray(I_dn).convert('L')
-    # I_dn.save('I_dn.jpg')
-
- ######################### PART 2 - 2c ##################################
-
-    # I_n = Image.open('./hw12024_input_img/hw12024/I_n.jpg')
-    # I_n = ImageOps.grayscale(I_n)
-
-    # I_dn2_x = cv2.Sobel(np.asarray(I_n), cv2.CV_64F, 1, 0)
-    # I_dn2_y = cv2.Sobel(np.asarray(I_n), cv2.CV_64F, 0, 1)
-
-    # I_dn2_sqrt = np.sqrt(I_dn2_x ** 2 + I_dn2_y ** 2)
-    # I_dn2 = I_dn2_sqrt*255/I_dn2_sqrt.max()
-
-    # I_dn2 = Image.fromarray(I_dn2).convert('L')
-    # I_dn2.save('I_dn2.jpg')
-
- ######################### PART 2 - 3a ##################################
-
-    I_n = Image.open('./hw12024_input_img/hw12024/I_n.jpg')
+    
+    # import image
+    I_n = Image.open('hw12024_input_img/hw12024/I_n.jpg')
     I_n = ImageOps.grayscale(I_n)
 
-    I_n_fft = np.fft.fft2(np.asarray(I_n))
+    # apply gaussian blur using 3x3 kernel
+    I_dn = cv2.GaussianBlur(np.asarray(I_n), (3, 3), 0)
+
+    # export image
+    I_dn = Image.fromarray(I_dn).convert('L')
+    I_dn.save('answers/I_dn.jpg')
+
+    ######################### PART 2 - 2c ##################################
+
+    # import image
+    I_n = Image.open('hw12024_input_img/hw12024/I_n.jpg')
+    I_n = ImageOps.grayscale(I_n)
+
+    # apply sobel inx and y directions
+    I_dn2_x = cv2.Sobel(np.asarray(I_n), cv2.CV_64F, 1, 0)
+    I_dn2_y = cv2.Sobel(np.asarray(I_n), cv2.CV_64F, 0, 1)
+
+    # normalize
+    I_dn2_sqrt = np.sqrt(I_dn2_x ** 2 + I_dn2_y ** 2)
+    I_dn2 = I_dn2_sqrt * 255 / I_dn2_sqrt.max()
+
+    # export image
+    I_dn2 = Image.fromarray(I_dn2).convert('L')
+    I_dn2.save('answers/I_dn2.jpg')
+
+    ######################### PART 2 - 3a ##################################
+
+    # retrieve amplitude and phase of I.jpg
+    amplitude_I, phase_I = calculate_amplitude_phase('hw12024_input_img/hw12024/I.jpg')
+
+    # normalize
+    amplitude_db_I = 20 * np.log10(amplitude_I + 1)
+    phase_normalized_I = 255 * (phase_I + np.pi) / (2 * np.pi)
+
+    # export images
+    I_amp = Image.fromarray(amplitude_db_I).convert('L')
+    I_amp.save('answers/I_amp.jpg')
+
+    I_phase = Image.fromarray(phase_normalized_I).convert('L')
+    I_phase.save('answers/I_phase.jpg')
+
+    # retrieve amplitude and phase of I_n.jpg
+    amplitude_In, phaseIn = calculate_amplitude_phase('hw12024_input_img/hw12024/I_n.jpg')
+
+    # normalize
+    amplitude_db_In = 20 * np.log10(amplitude_In + 1)
+    phase_normalized_In = 255 * (phaseIn + np.pi) / (2 * np.pi)
+
+    # export images
+    I_n_amp = Image.fromarray(amplitude_db_In).convert('L')
+    I_n_amp.save('answers/I_n_amp.jpg')
+
+    I_n_phase = Image.fromarray(phase_normalized_In).convert('L')
+    I_n_phase.save('answers/I_n_phase.jpg')
     
-    amplitude = np.abs(I_n_fft)
+    ######################### PART 2 - 3b ##################################
 
-    amplitude = (255 * (amplitude - np.min(amplitude)) / (np.max(amplitude) - np.min(amplitude))).astype(np.uint32)
-
-    print(amplitude)
+    # calculate the subtraction of both amplitudes
+    sub_amplitude = np.abs(amplitude_I - amplitude_In)
+    sub_amplitude_db = 20 * np.log10(sub_amplitude + 1)
     
-    phase = np.angle(I_n_fft)
+    # export image
+    sub_amplitude_img = Image.fromarray(sub_amplitude_db).convert('L')
+    sub_amplitude_img.save('answers/sub_amplitude.jpg')
 
-    I_n_amp = Image.fromarray(amplitude).convert('L')
-    I_n_amp.save('I_n_amp.jpg')
+    ######################### PART 2 - 3c ##################################
 
-    # 
- 
+    # retrieve amplitude of chita and phase of zebra
+    amplitude_chita, _ = calculate_amplitude_phase('hw12024_input_img/hw12024/chita.jpeg')
+    _, phase_zebra = calculate_amplitude_phase('hw12024_input_img/hw12024/zebra.jpeg')
 
+    # normalize
+    amplitude_db_chita = 20 * np.log10(amplitude_chita + 1)
+    phase_normalized_zebra = 255 * (phase_zebra + np.pi) / (2 * np.pi)
+
+    # export chita amplitude and zebra phase
+    chita_amp = Image.fromarray(amplitude_db_chita).convert('L')
+    chita_amp.save('answers/chita_amp.jpg')
+
+    zebra_phase = Image.fromarray(phase_normalized_zebra).convert('L')
+    zebra_phase.save('answers/zebra_phase.jpg')
+
+    ######################### PART 2 - 3d ##################################
+
+    # retrieve amplitude of chita and phase of zebra and crop them to be the same size
+    amplitude_chita, _ = calculate_amplitude_phase('hw12024_input_img/hw12024/chita.jpeg', (176, 225), True)
+    _, phase_zebra = calculate_amplitude_phase('hw12024_input_img/hw12024/zebra.jpeg', (176, 225), True)
+
+    # recover the image using inverse fft
+    recovered_fft = amplitude_chita * np.exp(1j * phase_zebra)
+    unshifted_fft = np.fft.ifftshift(recovered_fft)
+    recovered_img = np.real(np.fft.ifft2(unshifted_fft)).astype(np.uint8)
+
+    # export image
+    recovered_img = Image.fromarray(recovered_img).convert('L')
+    recovered_img.save('answers/recovered_zebra_chita.jpg')
+
+    pass
 
 if __name__ == '__main__':
     main()
